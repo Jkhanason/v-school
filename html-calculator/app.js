680f2c34db405e5473413602817ca02542390dba
Jason Khan <khanmanjr@yahoo.com>
2022-03-22 17:54:47 -0500

    reorganized folders

---
+++ html-calculator/app.js
@@ -0,0 +1,36 @@
+const addForm = document.add;
+
+addForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+  let num1 = addForm.num1.value;
+  let num2 = addForm.num2.value;
+  let total = +num1 + +num2;
+  document.getElementsByClassName('addResult')[0].textContent = total;
+  addForm.num1.value = '';
+  addForm.num2.value = '';
+
+});
+
+const subtractForm = document.subtract;
+
+subtractForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+  let num1 = subtractForm.num1.value;
+  let num2 = subtractForm.num2.value;
+  let total = +num1 - +num2;
+  document.querySelector('.subResult').textContent = total;
+  subtractForm.num1.value = '';
+  subtractForm.num2.value = '';
+});
+
+const multiplyForm = document.multiply;
+
+multiplyForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+  let num1 = multiplyForm.num1.value;
+  let num2 = multiplyForm.num2.value;
+  let total = +num1 * +num2;
+  document.querySelector('.multResult').textContent = total;
+  multiplyForm.num1.value = '';
+  multiplyForm.num2.value = '';
+});
