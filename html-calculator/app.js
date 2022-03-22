0a4327b14fd95ca834f0a4f36d2b14bd0af5810c
Jason Khan <khanmanjr@yahoo.com>
2022-03-22 17:58:27 -0500

    initial commit

--- html-calculator/app.js
+++ html-calculator/app.js
@@ -4,11 +4,10 @@ addForm.addEventListener('submit', (e) => {
   e.preventDefault()
   let num1 = addForm.num1.value;
   let num2 = addForm.num2.value;
-  let total = +num1 + +num2;
-  document.getElementsByClassName('addResult')[0].textContent = total;
+  let total = +num1 + +num2; //new syntax i found for doing math on strings
+  document.getElementsByClassName('addResult')[0].textContent = total; //used GEBCN here
   addForm.num1.value = '';
   addForm.num2.value = '';
-
 });
 
 const subtractForm = document.subtract;
@@ -18,7 +17,7 @@ subtractForm.addEventListener('submit', (e) => {
   let num1 = subtractForm.num1.value;
   let num2 = subtractForm.num2.value;
   let total = +num1 - +num2;
-  document.querySelector('.subResult').textContent = total;
+  document.querySelector('.subResult').textContent = total; //switched to QS here
   subtractForm.num1.value = '';
   subtractForm.num2.value = '';
 });
