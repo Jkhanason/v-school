93115b90e3bb1468ad1190211c9524d0a379e354
Jason Khan <khanmanjr@yahoo.com>
2022-04-05 15:03:33 -0500

    adding color and dislay grid styling

--- pokemon-list/app.js
+++ pokemon-list/app.js
@@ -13,10 +13,13 @@ xhr.send();
 
 const pokemonNames = array => {
   const details = ['is strong', 'is smart', 'is brave', 'is shy', 'is hard to catch', 'is a loner', 'is social'];
+  const classes = ['one', 'two', 'three'];
   array.forEach(current => {
-    const randomNum = Math.floor(Math.random() * 7);
+    const randomNum7 = Math.floor(Math.random() * 7);
+    const randomNum3 = Math.floor(Math.random() * 3);
     let newP = document.createElement('p');
-    newP.textContent = `${current.name.toUpperCase()} ${details[randomNum]}`;
+    newP.classList = classes[randomNum3];
+    newP.textContent = `${current.name.toUpperCase()} ${details[randomNum7]}`;
     document.body.append(newP);
   });
 }
