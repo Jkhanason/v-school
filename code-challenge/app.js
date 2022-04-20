9f89f135227a3cd7a99e0c154f3b1ff335ef8495
Jason Khan <khanmanjr@yahoo.com>
2022-04-20 13:08:15 -0500

    code challenge 4/20/22

--- code-challenge/app.js
+++ code-challenge/app.js
@@ -43,30 +43,46 @@
 
 
       //code challenge 2
-const animals = [
-	{
-		  name: "Spot",
-			species: "Dog",
-			weightInPounds: 200
-  },
-	{
-		  name: "Flute",
-			species: "Cat",
-			weightInPounds: 25
-  },
-	{
-		  name: "Trix",
-			species: "Dog",
-			weightInPounds: 15
-  },
-	{
-		  name: "Tabby",
-			species: "Cat",
-			weightInPounds: 9
-  }
-];
-const onlyCats = animals.filter(animal => animal.species === "Cat")
-console.log(onlyCats)
+// const animals = [
+// 	{
+// 		  name: "Spot",
+// 			species: "Dog",
+// 			weightInPounds: 200
+//   },
+// 	{
+// 		  name: "Flute",
+// 			species: "Cat",
+// 			weightInPounds: 25
+//   },
+// 	{
+// 		  name: "Trix",
+// 			species: "Dog",
+// 			weightInPounds: 15
+//   },
+// 	{
+// 		  name: "Tabby",
+// 			species: "Cat",
+// 			weightInPounds: 9
+//   }
+// ];
+// const onlyCats = animals.filter(animal => animal.species === "Cat")
+// console.log(onlyCats)
 
-const totalWeight = animals.reduce((total, animal) => total += animal.weightInPounds, 0)
-console.log(totalWeight)
+// const totalWeight = animals.reduce((total, animal) => total += animal.weightInPounds, 0)
+// console.log(totalWeight)
+
+
+const arrayOfMultiples = ((num, length) => {
+	const resultsArray = [];
+	let accumulator = num;
+
+	while (resultsArray.length < length) {
+		resultsArray.push(accumulator);
+		accumulator += num;
+	}
+	return resultsArray
+})
+
+console.log(arrayOfMultiples(7, 5))// ➞ [7, 14, 21, 28, 35]
+console.log(arrayOfMultiples(12, 10))// ➞ [12, 24, 36, 48, 60, 72, 84, 96, 108, 120]
+console.log(arrayOfMultiples(17, 6))// ➞ [17, 34, 51, 68, 85, 102]
